dad6d3d483a41d214fcd6b51f7a3ae345fdbf97e
rartigues2019 <rartigues2019@inf.udec.cl>
2022-06-10 05:49:32 -0400

    #11 -error on no .env

--- configuration/Settings.py
+++ configuration/Settings.py
@@ -1,10 +1,17 @@
+from ast import Raise
+import logging
 import os
+import traceback
 from dotenv import load_dotenv
 
 load_dotenv()
 
 class Settings:
-    IP = os.getenv("IP")
-    PORT = int(os.getenv("PORT"))
-    ADDR = (IP, PORT)
-    FORMAT = "utf-8"
+    try:
+        IP = os.getenv("IP")
+        PORT = int(os.getenv("PORT"))
+        ADDR = (IP, PORT)
+        FORMAT = "utf-8"
+    except Exception as e:
+        print("\n!!!!!!!!!Error: .env missing!!!!!!!!!")
+        raise e
